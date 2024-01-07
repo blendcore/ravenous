814e528f60a2c2e57cd743df46c1b0fdcd369f74
blendcore <blendcore@gmail.com>
2024-01-07 17:41:20 +0200

    Add Business Component, remove unused assets

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Business from './components/Business';
 
 function App() {
+  const [businessess, setBusinesses] = useState([
+    {
+      imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
+      name: 'MarginOtto Pizzeria',
+      address: '1010 Paddington Way',
+      city: 'Flavortown',
+      state: 'NY',
+      zipCode: '10101',
+      category: 'Italian',
+      rating: 4.5,
+      reviewCount: 90
+    },
+    {
+      imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
+      name: 'MarginOtto Pizzeria',
+      address: '1010 Paddington Way',
+      city: 'Flavortown',
+      state: 'NY',
+      zipCode: '10101',
+      category: 'Italian',
+      rating: 4.5,
+      reviewCount: 90
+    },
+    {
+      imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
+      name: 'MarginOtto Pizzeria',
+      address: '1010 Paddington Way',
+      city: 'Flavortown',
+      state: 'NY',
+      zipCode: '10101',
+      category: 'Italian',
+      rating: 4.5,
+      reviewCount: 90
+    }
+  ]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>
+        <div class="row">
+          {businessess.map((business) => (
+            <div class="col-lg-4">
+              <Business
+                business={business}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
