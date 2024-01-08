fdf0605cdaa71ca30cd7efbcf99d5f34f36cc542
blendcore <blendcore@gmail.com>
2024-01-08 19:35:34 +0200

    Style Components

--- src/components/BusinessList.js
+++ src/components/BusinessList.js
@@ -3,14 +3,17 @@ import Business from './Business';
 export default function BusinessList(props) {
     const { businessess } = props;
     return (
-        <div class="row">
-            {businessess.map((business) => (
-                <div class="col-lg-4">
-                    <Business
-                        business={business}
-                    />
-                </div>
-            ))}
+        <div class="container">
+            <h2 className='mb-5'>Our Businesses</h2>
+            <div class="row">
+                {businessess.map((business) => (
+                    <div class="col-lg-4">
+                        <Business
+                            business={business}
+                        />
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
