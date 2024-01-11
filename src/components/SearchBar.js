51655d84168dd6c043adff673593eba645dec247
blendcore <blendcore@gmail.com>
2024-01-11 18:30:26 +0200

    Search Bar sorting options improvements

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,23 +1,25 @@
 import './SearchBar.css';
 
+const sortByOptions = {
+    "Best Match": "best_match",
+    "Highest Rated": "rating",
+    "Most Reviewed": "review_count",
+};
+
 export default function SearchBar(props) {
+    const renderSortByOptions = () => {
+        return Object.keys(sortByOptions).map((sortByOption) => {
+            let sortByOptionValue = sortByOptions[sortByOption];
+            return <div>
+                <input className="btn-check" type="radio" name="sortingOptions" id={sortByOption} value={sortByOptionValue} />
+                <label className="btn btn-outline-light" for={sortByOption}>{sortByOption}</label>
+            </div>;
+        });
+    };
     return (
         <form className="search-bar" role="search">
             <div className='container'>
-                <div className='d-flex mb-3'>
-                    <div>
-                        <input className="btn-check" type="radio" name="sortingOptions" id="sortingOption1" value="Best Match" />
-                        <label className="btn btn-outline-light" for="sortingOption1">Best Match</label>
-                    </div>
-                    <div>
-                        <input className="btn-check" type="radio" name="sortingOptions" id="sortingOption2" value="Highest Rated" />
-                        <label className="btn btn-outline-light" for="sortingOption2">Highest Rated</label>
-                    </div>
-                    <div>
-                        <input className="btn-check" type="radio" name="sortingOptions" id="sortingOption3" value="Most Reviewed" />
-                        <label className="btn btn-outline-light" for="sortingOption3">Most Reviewed</label>
-                    </div>
-                </div>
+                <div className="d-flex mb-3">{renderSortByOptions()}</div>
                 <div className="d-sm-flex mb-3">
                     <input className="form-control form-control-lg me-sm-3 mb-3" type="search" placeholder="Search Businesses" aria-label="Search Businesses" />
                     <input className="form-control form-control-lg ms-sm-3 mb-3" type="search" placeholder="Where?" aria-label="Where?" />
