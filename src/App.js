affe1f4f011d15ef03a80a5883fffd8d8a849922
blendcore <blendcore@gmail.com>
2024-01-12 16:41:14 +0200

    Refactor all the components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import { useState } from "react";
+
 import Header from "./components/Header";
 import SearchBar from "./components/SearchBar";
 import BusinessList from "./components/BusinessList";
 
-function App() {
+const App = () => {
   const [businessess, setBusinesses] = useState([
     {
       imageSrc: 'https://images.unsplash.com/photo-1590947132387-155cc02f3212?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
