1b484d11870a6f123524bf47a83eea7917efb14d
blendcore <blendcore@gmail.com>
2024-01-12 18:37:52 +0200

    Add initial state to the Search Bar Component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './SearchBar.css';
 
 const sortByOptions = {
@@ -7,6 +8,10 @@ const sortByOptions = {
 };
 
 const SearchBar = () => {
+    const [search, setSearch] = useState();
+    const [location, setLocation] = useState();
+    const [sortByOption, setSortByOption] = useState("best_match");
+
     const renderSortByOptions = () => {
         return Object.keys(sortByOptions).map((sortByOption) => {
             let sortByOptionValue = sortByOptions[sortByOption];
@@ -16,13 +21,27 @@ const SearchBar = () => {
             </div>;
         });
     };
+
+    const handleSearchChange = (e) => {
+        setSearch(e.target.value);
+    }
+
+    const handleLocationChange = (e) => {
+        setLocation(e.target.value);
+    }
+
+    const handleSearch = (e) => {
+        console.log(`Searching Yelp with ${search}, ${location}, ${sortByOption}`);
+        e.preventDefault();
+    }
+
     return (
-        <form className="search-bar" role="search">
+        <form className="search-bar" role="search" onSubmit={handleSearch}>
             <div className='container'>
                 <div className="d-flex mb-3">{renderSortByOptions()}</div>
                 <div className="d-sm-flex mb-3">
-                    <input className="form-control form-control-lg me-sm-3 mb-3" type="search" placeholder="Search Businesses" aria-label="Search Businesses" />
-                    <input className="form-control form-control-lg ms-sm-3 mb-3" type="search" placeholder="Where?" aria-label="Where?" />
+                    <input className="form-control form-control-lg me-sm-3 mb-3" type="search" placeholder="Search Businesses" aria-label="Search Businesses" value={search} onChange={handleSearchChange} />
+                    <input className="form-control form-control-lg ms-sm-3 mb-3" type="search" placeholder="Where?" aria-label="Where?" value={location} onChange={handleLocationChange} />
                 </div>
                 <button className="btn btn-warning btn-lg mx-auto d-block" type="submit">Let's go</button>
             </div>
